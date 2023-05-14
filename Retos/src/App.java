419cbf68d352d9d4a37472cf84950943a548e219
Santiago Bulla Perez <133416259+SantiagoBulla@users.noreply.github.com>
2023-05-13 23:24:45 -0500

    Update class app data
    Update the information inside java class App

--- Retos/src/App.java
+++ Retos/src/App.java
@@ -2,9 +2,6 @@ import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Random r = new Random();
-        int valor = r.nextInt(2);
-        System.out.println(valor);
+        System.out.println("Este proyecto ya fue subido al GitHub");
     }
 }
